217b7353e4c0017e3a9c9a20fc1e34a8120b7794
Rufai-Ahmed <abbeyrufai234@gmail.com>
2023-11-10 15:37:36 +0100

    mine

--- index.ts
+++ index.ts
@@ -5,3 +5,13 @@ const port: number = 6666;
 const app: Application = express();
 
 console.log("Boom");
+
+process.on("uncaughtException", (err) => {
+  console.log("uncaughtException: ", err);
+  process.exit(1);
+});
+
+process.on("unhandledRejection", (reason: any) => {
+  console.log("unhandledRejection: ", reason);
+  process.exit(1);
+});
